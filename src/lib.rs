a8f413ffdca462a3eb4cfa18fd7c2063b9f6f818
devuan <devuan@devuan>
2022-02-23 17:16:11 +0100

    add ultrasonic sensor

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
+#[macro_use] extern crate log;
+
 mod servo;
 mod mpu6050;
 mod laser;
 mod colour;
 mod motors;
+mod ultrasonic;
 
 pub use {
     servo::*,
@@ -10,14 +13,18 @@ pub use {
     laser::*,
     colour::*,
     motors::*,
+    ultrasonic::*,
 };
+
 
 pub fn adjust_pwm_value(mut value: f64) -> f64 {
     if value <= 0.0 || value >= 1.0 {
         if value >= 1.0 {
+            warn!("[rasp_driver_collection/adjust_pwm_value]: PWM VALUE TOO HIGH, ADJUSTING DOWN!");
             value = 1.0;
         }
         else {
+            warn!("[rasp_driver_collection/adjust_pwm_value]: PWM VALUE TOO LOW, ADJUSTING UP!");
             value = 0.0;
         }
     }
